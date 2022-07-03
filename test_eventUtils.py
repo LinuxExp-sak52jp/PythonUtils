073f7759b957f936e6b07269e8228f7082657658
Shigeru Sakamoto <charan@mx2.ttcn.ne.jp>
2022-07-03 21:54:35 +0900

    wait_all()のテスト完了

--- test_eventUtils.py
+++ test_eventUtils.py
@@ -4,6 +4,7 @@ eventUtils.pyのテストケース
 import time
 import threading
 from threadUtils import eventUtil
+import sys
 
 
 def task(event, name, sleepTime):
@@ -23,9 +24,15 @@ b = threading.Event()
 
 threading.Thread(target=task,name='TaskA',args=(a,'TaskA',3)).start()
 threading.Thread(target=task,name='TaskB',args=(b,'TaskB',5)).start()
+r = eventUtil.wait_all((a,b))
+print(f"All events are set:{r}")
 
-eventUtil.wait_all((a,b))
-
-print('All events are set')
-
-
+a.clear()
+b.clear()
+threading.Thread(
+    target=task,name='TaskA',args=(a,'TaskA',3),daemon=True).start()
+threading.Thread(
+    target=task,name='TaskB',args=(b,'TaskB',5),daemon=True).start()
+r = eventUtil.wait_all((a,b),1)
+print(f"Timeout wait_all()->{r}")
+sys.exit(0)
--- threadUtils/eventUtil.py
+++ threadUtils/eventUtil.py
@@ -1,19 +1,40 @@
-#from multiprocessing.dummy import Event
-from threading import Thread
+from threading import Thread, current_thread
 from time import sleep, time
 import threading
 
-def __waitEventTask(event, barrier, idx):
-    print("Waiting event{}".format(idx))
-    event.wait()
-    print("event{} Set".format(idx))
-    barrier.wait()
-    
+def __waitEventAllTask(event: threading.Event, barrier):
+    print(f"Waiting event{current_thread().name}")
+    r = event.wait()
+    print(f"event{current_thread().name} Set")
+    try:
+        barrier.wait()
+    except:
+        return
 
-def wait_all(events, timeout=0):
-    b = threading.Barrier(len(events)+1)
+
+def wait_all(events, timeover=None) -> bool:
+    """全てのイベントがセットされるまで待機する
+
+    Args:
+        events (tuppel(Event)): 待機するEvent flagのリスト
+        timeover (float, optional): タイムアウト時間. Defaults to None.
+
+    Returns:
+        bool: 全てのイベントが発火したらTrue、Timeout発生時はFalse。
+    """
+    b = threading.Barrier(len(events)+1,timeout=timeover)
     idx = 0
     for e in events:
-        Thread(target=__waitEventTask,args=(e,b,idx)).start()
+        t = Thread(
+            target=__waitEventAllTask,
+            args=(e,b),name=f"{idx}",daemon=True).start()
         idx += 1
-    b.wait()
+    try:
+        b.wait()
+    except threading.BrokenBarrierError:
+        print("Timeout 発生!")
+        return False
+    return True
+
+def wait_any(events,timeover=None) -> int:
+    pass
